68799f833e9e9e30362f760d7de92042b9af4775
Jorge Mendes <jorge.mendes@mezu.com>
2021-08-10 21:50:49 +0100

    0.9.0

--- Package.swift
+++ Package.swift
@@ -10,19 +10,11 @@ let package = Package(
     products: [
         .library(name: "PaymentsSDK", targets: ["PaymentsSDK"])
     ],
-    dependencies: [
-        .package(name: "AlCore", url: "https://github.com/Alviere/alviere-core-ios.git", from: "0.9.0")
-    ],
     targets: [
         .binaryTarget(
             name: "Payments",
             url: "https://github.com/Alviere/alviere-payments-ios/releases/download/0.9.0/Payments.xcframework.zip",
             checksum: "d12c5cbf12d5e3b108503efa26e7e49d4a14a3b17c63530135bed879b2390a04"
-        ),
-        .target(
-            name: "PaymentsSDK",
-            dependencies: ["AlCore", "Payments"],
-            path: "PaymentsSDK"
         )
     ]
 )
